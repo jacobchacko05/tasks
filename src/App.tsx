17197865ff380e7ee99efc554820042845eadebe
Jacob Chacko <jchacko@udel.edu>
2026-02-10 13:57:25 -0500

    completed html tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,12 +8,52 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>76ers</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <div>Jacob Chacko</div>
             <div>Hello World</div>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/Philadelphia_76ers_logo.svg/250px-Philadelphia_76ers_logo.svg.png"
+                alt="76ers logo"
+            />
+            <ul>
+                <li>Embiid</li>
+                <li>Maxey</li>
+                <li>Edgecome</li>
+            </ul>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
